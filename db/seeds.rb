48a5ebbc83931bdf86df0b652e58e537d10595a5
Diego Bolettieri <diego@bebetterhotels.com>
2018-11-09 14:24:09 +0100

    finished routes and controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+# Ingredient.create(name: 'lemon')
+# Ingredient.create(name: 'ice')
+# Ingredient.create(name: 'mint leaves')
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+drinks = JSON.parse(ingredients_serialized)
+ingredients = drinks.flatten(2)
+ingredients.delete_at(0)
+ingredients.each do |ingredient|
+  ingredient.each_value do |value|
+    Ingredient.create!(name: value)
+  end
+end
